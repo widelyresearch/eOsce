407f053200f7a8b96cd62dc075641d321d2d201f
Nicolás Troncoso <hi@nicotron.cl>
2019-05-01 20:21:25 -0300

    build basic structure

--- chatbot00/sketch.js
+++ chatbot00/sketch.js
@@ -7,32 +7,18 @@
 function setup() {
   noCanvas();
 
-  let bot = new RiveScript();
-  bot.loadFile("brain.rive", brainReady, brainError);
-
-  function brainReady() {
-    console.log('Chatbot ready!');
-    bot.sortReplies();
-    let num = floor(random(10)) + 1;
-    console.log(num);
-    let reply = bot.reply('local-user', 'set ' + num);
-  }
-
-  function brainError() {
-    console.log('Chatbot error!')
-  }
-
-
-  let button = select('#submit');
+  let button = select('#submit'); //
   let user_input = select('#user_input');
   let output = select('#output');
 
   button.mousePressed(chat);
 
   function chat() {
+    // gettin the value of the variable user_input made by keyboard
     let input = user_input.value();
-    let reply = bot.reply("local-user", input);
-    output.html(reply);
+    // displaying that new variable in the html page
+    output.html(input);
   }
+
 
 }
